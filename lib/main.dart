4c1bcbc898c0ec7d56e5f460094d206a4f3d2f86
Antfeeder <jesuskevinsilva@gmail.com>
2021-08-05 00:46:49 -0700

    Added basic API call to "Spells" Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert' show jsonDecode;
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const Spellz());
@@ -24,7 +27,7 @@ class Spellz extends StatelessWidget {
 
 
 /// This is the stateless widget that the main application instantiates.
-class AppMenu extends StatelessWidget {
+/*class AppMenu extends StatelessWidget {
   const AppMenu({Key? key}) : super(key: key);
 
   @override
@@ -54,7 +57,7 @@ class AppMenu extends StatelessWidget {
                     primary: Colors.white,
                     textStyle: const TextStyle(fontSize: 20),
                   ),
-                  onPressed: () {},
+                  onPressed: _pushSaved(context),
                   child: const Text('Spells'),
                 ),
               ],
@@ -109,6 +112,167 @@ class AppMenu extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}*/
+
+class AppMenu extends StatefulWidget {
+  const AppMenu({Key? key}) : super(key: key);
+
+  @override
+  _AppMenuState createState() => _AppMenuState();
+}
+
+class _AppMenuState extends State<AppMenu> {
+  late Future<Spell> futureSpell;
+
+  @override
+  void initState() {
+    super.initState();
+    futureSpell = fetchSpell();
+  }
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text('Spellz',
+              style: TextStyle(fontSize: 72)
+          ),
+          const SizedBox(height: 30),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: const Color(0xFF0D47A1),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(16.0),
+                    primary: Colors.white,
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  onPressed: _pushSaved,
+                  child: const Text('Spells'),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 30),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: const Color(0xFF0D47A1),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(16.0),
+                    primary: Colors.white,
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Spellbooks'),
+                ),
+              ],
+            ),
+          ),const SizedBox(height: 30),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(4),
+            child: Stack(
+              children: <Widget>[
+                Positioned.fill(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: const Color(0xFF0D47A1),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    padding: const EdgeInsets.all(16.0),
+                    primary: Colors.white,
+                    textStyle: const TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Options'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // NEW lines from here...
+        builder: (BuildContext context) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: Center(
+                child: FutureBuilder<Spell>(
+                  future: futureSpell,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Text(snapshot.data!.name);
+                    } else if (snapshot.hasError) {
+                      return Text('${snapshot.error}');
+                    }
+
+                    // By default, show a loading spinner.
+                    return const CircularProgressIndicator();
+                  },
+                )
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+
+
+
+Future<Spell> fetchSpell() async {
+  final response = await http
+      .get(Uri.parse('https://www.dnd5eapi.co/api/spells/acid-arrow'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Spell.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load Spell');
+  }
+}
+
+class Spell {
+  final String name;
+
+  Spell({
+    required this.name,
+  });
+
+  factory Spell.fromJson(Map<String, dynamic> json) {
+    return Spell(
+      name: json['name'],
     );
   }
 }
